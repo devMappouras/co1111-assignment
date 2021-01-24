68db49f0216dd1743fa842dd08f7c22a1db1cec1
Chris Mappouras <57260161+Pelopedis@users.noreply.github.com>
2021-01-24 12:18:25 +0200

    created getTreasureHunts function

--- thcode.js
+++ thcode.js
@@ -1,1 +1,27 @@
 
+//calls treasure hunts from API
+//creates a list with all treasure hunts
+function getTreasureHunts() {
+
+    fetch("https://codecyprus.org/th/api/list")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+
+            let playersName=prompt("Enter Your Name:", "");
+
+            let thList = document.getElementById("thList");
+            let treasureHunts = jsonObject.treasureHunts;
+
+            for(let i=0; i < treasureHunts.length; i++) {
+                let  id = treasureHunts[i].uuid;
+
+                let listItem = document.createElement("li");
+                let treasureHuntName =  treasureHunts[i].name;
+                //listItem.innerHTML = treasureHuntName;
+
+                listItem.innerHTML = "<a href='https://codecyprus.org/th/api/start?player="+ playersName +"&app=team3TreasureHunt&treasure-hunt-id="+id+"'>" + treasureHuntName + "</a>";
+
+                thList.appendChild(listItem);
+            }
+        });
+}
