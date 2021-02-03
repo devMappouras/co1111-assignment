f67dda5c69ae8a747bd8b462ae0d1109ebe129f4
Chris Mappouras <57260161+Pelopedis@users.noreply.github.com>
2021-02-03 21:30:28 +0200

    added start service funcitonality

--- thcode.js
+++ thcode.js
@@ -19,9 +19,61 @@ function getTreasureHunts() {
                 let treasureHuntDesc =  treasureHunts[i].description;
                 let treasureHuntID =  treasureHunts[i].uuid;
 
-                listItem.innerHTML ="<div class='container2'>" + "<h4>" +treasureHuntName+ "</h4>"+ "<p>" +treasureHuntDesc+ "</p>" + "<a href='https://codecyprus.org/th/api/start?player="+ playersName +"&app=team3TreasureHunt&treasure-hunt-id="+treasureHuntID+"'>Start</a>" + "</div><br>";
+                //listItem.innerHTML ="<div class='container2'>" + "<h4>" +treasureHuntName+ "</h4>"+ "<p>" +treasureHuntDesc+ "</p>" + "<a href='https://codecyprus.org/th/api/start?player="+ playersName +"&app=team3TreasureHunt&treasure-hunt-id="+treasureHuntID+"'>Start</a>" + "</div><br>";
+                listItem.innerHTML ="<div class='container2'>" + "<h4>" +treasureHuntName+ "</h4>"+ "<p>" +treasureHuntDesc+ "</p>" + "<a href='question.html?player="+ playersName +"&treasure-hunt-id="+treasureHuntID+"'>Start</a>" + "</div><br>";
 
                 thList.appendChild(listItem);
             }
         });
+}
+
+//function that starts the th game
+function getStartData() {
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const playersName = urlParams.get('player')
+    let treasureHuntID = urlParams.get('treasure-hunt-id')
+
+    console.log(playersName);
+    console.log(treasureHuntID);
+
+    fetch("https://codecyprus.org/th/api/start?player="+ playersName +"&app=team3TreasureHunt&treasure-hunt-id="+treasureHuntID+"")
+        .then(response => response.json()) //Parse JSON text to JavaScript object
+        .then(jsonObject => {
+
+            let thstatus = jsonObject.status;
+            let thsession = jsonObject.session;
+            let totalQuestions = jsonObject.numOfQuestions;
+
+            if(thstatus==="OK") {
+                //gets questions from server and shows them to the user
+                getQuestion(thsession);
+
+            } else {
+                document.write(jsonObject.errorMessages);
+            }
+
+            console.log(thsession);
+            console.log(totalQuestions);
+
+        });
+}
+
+//function that calls the questions from server
+function getQuestion(thsession) {
+    let questionNo = 1;
+
+    let submitString = document.getElementById("submitString");
+    let answerString = document.getElementById("answerString");
+
+    let submitNo = document.getElementById("submitNo");
+    let answerNo = document.getElementById("answerNo");
+
+    var boolF = document.getElementById("false");
+    var boolT = document.getElementById("true");
+
+    let message = document.getElementById("message");
+
+
 }
