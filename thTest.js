d8bbf7d02d44fe0af217db3d52e2d0e3ea0e0309
Marina Irakleous <56921975+MIrakleous6@users.noreply.github.com>
2021-03-10 23:51:05 +0200

    updated listClear() and scoreClear()

--- thTest.js
+++ thTest.js
@@ -36,19 +36,27 @@ function listTest() {
                 let thEmailResults = treasureHunts[i].emailResults;
                 let thHasPrize = treasureHunts[i].hasPrize;
 
-                thList.innerHTML ="<div id='liID'>" + "<p>" +thName+ "</p>"+ "<p>" +thDesc+ "</p>" + "<p>" +thOwnerEmail+ "</p>" + "<p>" +thSecretCode+ "</p>" + "<p>" +thSalt+ "</p>" + "<p>" +thVisibility+ "</p>" + "<p>" +thStartsOn+ "</p>" + "<p>" +thEndsOn+ "</p>" + "<p>" +thMaxDuration+ "</p>" + "<p>" +thShuffled+ "</p>" + "<p>" +thAuthentication+ "</p>" + "<p>" +thEmailResults+ "</p>" + "<p>" +thHasPrize+ "</p>" + "</div><br>";
+                thList.innerHTML ="<div class='liClass'>" + "<p>" +thName+ "</p>"+ "<p>" +thDesc+ "</p>" + "<p>" +thOwnerEmail+ "</p>" + "<p>" +thSecretCode+ "</p>" + "<p>" +thSalt+ "</p>" + "<p>" +thVisibility+ "</p>" + "<p>" +thStartsOn+ "</p>" + "<p>" +thEndsOn+ "</p>" + "<p>" +thMaxDuration+ "</p>" + "<p>" +thShuffled+ "</p>" + "<p>" +thAuthentication+ "</p>" + "<p>" +thEmailResults+ "</p>" + "<p>" +thHasPrize+ "</p>" + "<br>"+ "</div>";
 
                 listTestOutput.appendChild(thList);
             }
         });
     }
 
-//calls treasure hunts from API
-//creates a list with all treasure hunts
+
 function listClear() {
 
     let listTestOutput = document.getElementById("listTestOutput");
     listTestOutput.style.display="none";
+    /*class is used than ID because the function listTest() can create more than one li-item,
+     therefore an ID could not be used since it specifies a unique ID for an element*/
+    //returns all elements by className as HTML collection object, each object has an index(starts at 0)
+    let testDelete = document.getElementsByClassName("liClass");
+    //while is used to delete the objects one by one until the length of the collection is 0
+    while(testDelete.length>0){
+        testDelete[0].parentNode.removeChild(testDelete[0]);
+    }
+  //  testDelete.parentNode.removeChild(testDelete);
 }
 
 function scoreTest(){
@@ -76,7 +84,7 @@ fetch("https://codecyprus.org/th/test-api/score?score=" + answer)
 
 
 
-       thScoreTest.innerHTML ="<div id='liID'>" + "<p>" +thCompleted+ "</p>"+ "<p>" +thFinished+ "</p>" + "<p>" +thPlayer+ "</p>" + "<p>" +thScore+ "</p>" + "<p>" +thHasPrize+ "</p>" + "<p>" +thStatus+ "</p>" + "</div><br>";
+       thScoreTest.innerHTML ="<div id='liID'>" + "<p>" +thCompleted+ "</p>"+ "<p>" +thFinished+ "</p>" + "<p>" +thPlayer+ "</p>" + "<p>" +thScore+ "</p>" + "<p>" +thHasPrize+ "</p>" + "<p>" +thStatus+ "</p>" + "<br>"+ "</div>";
 
        scoreTestOutput.appendChild(thScoreTest);
 
@@ -84,6 +92,13 @@ fetch("https://codecyprus.org/th/test-api/score?score=" + answer)
 }
 
 function scoreClear(){
-    let scoreTestOutput = document.getElementById("scoreTestOutput");
-    scoreTestOutput.style.display="none";
+   let scoreTestOutput = document.getElementById("scoreTestOutput");
+   //removeChild method is used to delete children (liID) of a parent (e.g scoreTestOutput)
+    //property parentNode is used to return the parent-node of the element
+   let testDelete = document.getElementById("liID");
+    testDelete.parentNode.removeChild(testDelete);
+
+   scoreTestOutput.style.display="none";
+
+
 }
